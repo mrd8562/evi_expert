23f4020c6c835c394935ac020b3bcf59beb912b5
fifmiller <sgleeb@gmail.com>
2025-03-21 16:29:45 +0300

    faq

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -35,3 +35,26 @@ export function ckMobile() {
     
   }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('.Gfaq__question').forEach(button => {
+      button.addEventListener('click', () => {
+          const item = button.parentElement;
+          const answer = item.querySelector('.Gfaq__answer');
+
+          if (item.classList.contains('Gfaq__item--active')) {
+              answer.style.maxHeight = null;
+              item.classList.remove('Gfaq__item--active');
+          } else {
+              document.querySelectorAll('.Gfaq__item--active').forEach(activeItem => {
+                  activeItem.querySelector('.Gfaq__answer').style.maxHeight = null;
+                  activeItem.classList.remove('Gfaq__item--active');
+              });
+
+              answer.style.maxHeight = answer.scrollHeight + "px";
+              item.classList.add('Gfaq__item--active');
+          }
+      });
+  });
+});
