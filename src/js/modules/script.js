1ad0982041008e40fc52d45ae687d6d680e0d63e
Maxim Ivanko <qzwxecrvtbasdf@gmail.com>
2025-03-23 12:58:37 +0300

    index page

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -30,8 +30,39 @@ export function ckMobile() {
   
 
   } else {
-   
     document.body.classList.add('_pc');
     
   }
 }
+
+const burgerMenu = document.getElementById('burgerMenu');
+const navLinks = document.getElementById('navLinks');
+const contactInfo = document.getElementById('contactInfo');
+const body = document.body;
+
+burgerMenu.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  contactInfo.classList.toggle('active');
+  burgerMenu.classList.toggle('open');
+  body.classList.toggle('blur');
+});
+
+// Получаем все элементы с классом dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+
+// Проходим по каждому элементу dropdown
+dropdowns.forEach(dropdown => {
+  const dropdownLink = dropdown.querySelector('a');
+  const dropdownContent = dropdown.querySelector('.dropdown-content');
+
+  // Добавляем слушатель клика на ссылку внутри dropdown
+  dropdownLink.addEventListener('click', (e) => {
+    // Проверяем размер экрана (меньше 876px)
+    if (window.innerWidth <= 876) {
+      e.preventDefault(); // Предотвращаем переход по ссылке
+
+      // Переключаем класс .show на dropdown-content
+      dropdownContent.classList.toggle('show');
+    }
+  });
+});
